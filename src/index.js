db21e86152b221a2cf5ed13d030b53a4c7c90a0c
Jose Francisco Villalobos Diaz <francisco.77.fd@live.com>
2023-06-22 09:53:03 -0600

    archivo para conexion con la base de datos

--- src/index.js
+++ src/index.js
@@ -9,3 +9,34 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'containers-us-west-185.railway.app',
+  port: '5997',
+  user: 'root',
+  password: 'ktwq47YGizZKhTtVDkvb',
+  database: 'railway',
+});
+
+// Establecer conexión
+connection.connect((error) => {
+  if (error) {
+    console.error('Error al conectar a la base de datos:', error);
+  } else {
+    console.log('Conexión exitosa a la base de datos.');
+
+    // Realizar consulta
+    connection.query('SELECT * FROM tabla', (error, results) => {
+      if (error) {
+        console.error('Error al ejecutar la consulta:', error);
+      } else {
+        console.log('Resultados:', results);
+      }
+
+      // Cerrar conexión después de la consulta
+      connection.end();
+    });
+  }
+});
